7422590a4d489a30628cae6344ffbf47af4439d7
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-27 20:49:10 +0100

    div change

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,15 @@
+import memesData from "../../memesData";
+
 function Meme() {
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    let randomNum = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNum].url;
+    console.log(url);
+  }
   return (
     <main className="main">
-      <form className="form">
+      <div className="form">
         <input
           type="text"
           className="form--input"
@@ -12,8 +20,10 @@ function Meme() {
           className="form--input"
           placeholder="Bottom text"
         ></input>
-        <button className="form--button">Get a new meme image</button>
-      </form>
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image
+        </button>
+      </div>
     </main>
   );
 }
