ed38b50d4c7a96bb8142e6ec78013266d95bfa57
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-27 16:28:59 +0100

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <img src="./src/assets/trollface.png" className="header--img"></img>
+      <h1 className="header--title">Meme Generator</h1>
+    </header>
+  );
+}
+
+export default Header;
