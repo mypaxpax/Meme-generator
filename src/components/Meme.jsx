bfb8fdd5cf9073e39db8193018edbcd7b8e66b25
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-02-04 15:27:49 +0100

    Image text in useState

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -8,6 +8,14 @@ function Meme() {
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
+  function eventHandle(event) {
+    const { name, value } = event.target;
+    setMeme((prevText) => ({
+      ...prevText,
+      [name]: value,
+    }));
+  }
+
   const [allMemesImages, setAllMemeImages] = React.useState(memesData);
 
   function getMemeImage() {
@@ -24,17 +32,27 @@ function Meme() {
           type="text"
           className="form--input"
           placeholder="Top text"
+          name="topText"
+          value={meme.topText}
+          onChange={eventHandle}
         ></input>
         <input
           type="text"
           className="form--input"
           placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={eventHandle}
         ></input>
         <button onClick={getMemeImage} className="form--button">
           Get a new meme image
         </button>
       </div>
-      <img src={meme.randomImage} className="meme--image"></img>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image"></img>
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
